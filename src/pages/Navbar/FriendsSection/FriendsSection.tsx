0babbef874e86b061deb4f73cdafe8664e253295
Aleksey <easy.mail.to.copy@gmail.com>
2023-12-20 00:38:34 +0300

    refactor: rework styles for navbar and friends section

--- src/pages/Navbar/FriendsSection/FriendsSection.tsx
+++ src/pages/Navbar/FriendsSection/FriendsSection.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import s from "./FriendsSection.module.css";
-import {UsersDataType} from "../../../redux/messagesReducer";
+import {UsersDataType} from "redux/messagesReducer";
 
 
 type FriendsSectionPropsType = {
@@ -9,10 +9,10 @@ type FriendsSectionPropsType = {
 export const FriendsSection = ({friendsData}: FriendsSectionPropsType) => {
     return (
         <div className={s.friendSection}>
-            <h3 className={s.title}>Friends</h3>
+            <h3 className={s.label}>Friends</h3>
             {friendsData.map(fd =>
-                <div key={fd.id}>
-                    <img src={fd.photoAvatar} alt="#"/> <span>{fd.animalName}</span>
+                <div className={s.item} key={fd.id}>
+                    <img className={s.avatar} src={fd.photoAvatar} alt="#"/> <span>{fd.animalName}</span>
                 </div>
             ).slice(0, 3)}
 
--- src/pages/Navbar/Navbar.tsx
+++ src/pages/Navbar/Navbar.tsx
@@ -4,39 +4,38 @@ import {UsersDataType} from "redux/messagesReducer";
 import {ReducersType} from 'redux/reduxStore';
 import {FriendsSection} from "./FriendsSection/FriendsSection";
 import s from './Navbar.module.css';
+import React from "react";
 
 
 export const Navbar = () => {
     const friendsData = useSelector<ReducersType, UsersDataType[]>(state => state.messagesData.messagesUsersData)
     return (
         <nav className={s.navbar}>
+            <h3 className={s.label}>Menu</h3>
             <div className={s.navButtonsSection}>
-                <div>
-                    <NavLink to='/profile/' className={navData => navData.isActive ? s.active : s.item}>Profile</NavLink>
-                </div>
-                <div>
+                <span>
+                    <NavLink to='/profile/' className={s.item}>Profile</NavLink>
+                </span>
+                <span>
                     <NavLink to='/messages'
-                             className={navData => navData.isActive ? s.active : s.item}>Message</NavLink>
-                </div>
-                <div>
+                             className={s.item}>Message</NavLink>
+                </span>
+                <span>
                     <NavLink to='/users'
-                             className={navData => navData.isActive ? s.active : s.item}>Users</NavLink>
-                </div>
-                <div>
-                    <NavLink to='/news' className={navData => navData.isActive ? s.active : s.item}>News</NavLink>
-                </div>
-                <div>
-                    <NavLink to='/music' className={navData => navData.isActive ? s.active : s.item}>Music</NavLink>
-                </div>
-                <div>
+                             className={s.item}>Users</NavLink>
+                </span>
+                <span>
+                    <NavLink to='/news' className={s.item}>News</NavLink>
+                </span>
+                <span>
+                    <NavLink to='/music' className={s.item}>Music</NavLink>
+                </span>
+                <span>
                     <NavLink to='/settings'
-                             className={navData => navData.isActive ? s.active : s.item}>Settings</NavLink>
-                </div>
+                             className={s.item}>Settings</NavLink>
+                </span>
             </div>
-            <div className={s.navFriendSection}>
                 <FriendsSection friendsData={friendsData}/>
-            </div>
-
         </nav>
     )
 }

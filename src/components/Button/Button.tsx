749ba5b620a3e2b6be4eec57b091f4b028e1552c
Aleksey <easy.mail.to.copy@gmail.com>
2023-12-23 21:44:43 +0300

    feat: added button component

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,26 @@
+import { ComponentPropsWithoutRef, ElementType, ForwardedRef, forwardRef } from 'react'
+
+import s from './Button.module.scss'
+
+export type ButtonProps<T extends ElementType = 'button'> = {
+  as?: T
+  variant?: 'primary' | 'secondary' | 'tertiary' | 'link'
+  fullWidth?: boolean
+} & ComponentPropsWithoutRef<T>
+
+const Button = <T extends ElementType = 'button'>(
+  props: ButtonProps<T>,
+  ref: ForwardedRef<HTMLButtonElement>
+) => {
+  const { variant = 'primary', fullWidth, className, as: Component = 'button', ...rest } = props
+
+  return (
+    <Component
+      className={`${s[variant]} ${fullWidth ? s.fullWidth : ''} ${className}`}
+      {...rest}
+      ref={ref}
+    />
+  )
+}
+
+export default forwardRef(Button)

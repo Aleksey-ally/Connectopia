4a63c7adc8485c658615b456457e518acc6e40b1
Aleksey <easy.mail.to.copy@gmail.com>
2023-12-31 20:44:36 +0300

    feat: added dropdown menu for header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -7,6 +7,9 @@ import {Typography} from "components/Typography/Typography";
 import {CICon} from "assets/icons/CIcon";
 import {Dropdown} from "components/Dropdown";
 import {DropdownItemWithIcon} from "components/Dropdown/DropdownItem/DropdownItemWithIcon";
+import PersonOutline from "assets/icons/person-outline";
+import Logout from "assets/icons/logout";
+import {DropdownItem} from "components/Dropdown/DropdownItem";
 
 type Props = {
     auth: Auth
@@ -24,9 +27,30 @@ export const Header = ({auth}: Props) => {
 
             <div className={s.loginBlock}>
                 {auth.isAuth ?
-                    <span>Hello <b>{auth.login}</b><UserAvatar size={'small'}/></span>
-                :
-                <NavLink className={s.login} to='/login'>Login</NavLink>}
+                    <>
+                        <Dropdown trigger={<span>Hello <b>{auth?.login}</b><UserAvatar size={'small'}/></span>}>
+                            <DropdownItem>
+                                <UserAvatar
+                                    photos={
+                                        'https://fikiwiki.com/uploads/posts/2022-02/1644918620_17-fikiwiki-com-p-krasivie-kartinki-visokogo-razresheniya-19.jpg'
+                                    }
+                                    size={'small'}
+                                />
+
+                                <div>
+                                    <Typography variant="subtitle2"> {auth?.login ? auth.login : 'User Name'}</Typography>
+                                    <Typography variant="caption" style={{color: 'var(--color-dark-100)'}}>
+                                        {auth?.email ? auth.email : 'User Name'}
+                                    </Typography>
+                                </div>
+                            </DropdownItem>
+                            <DropdownItemWithIcon icon={<PersonOutline/>} text={'My Profile'}/>
+                            <DropdownItemWithIcon icon={<Logout/>} text="Sign Out"/>
+                        </Dropdown>
+                    </> :
+                    <NavLink className={s.login} to='/login'>Login</NavLink>
+
+                }
             </div>
         </header>
     )

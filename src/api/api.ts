3c0b4d6d80163bc5dc873859f37887c30fdba1fa
Aleksey <easy.mail.to.copy@gmail.com>
2024-01-20 19:06:23 +0300

    refactor: relocated types from api to module

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,50 +1,5 @@
 import axios from "axios";
-import {UserType} from "redux/usersReducer";
-
-type ResponseUsersType = {
-    items: UserType[]
-    totalCount: number
-    error: null | number
-}
-
-type ResponseAuth = {
-    data: {
-        id: number
-        login: string
-        email: string
-    }
-    messages: string[]
-    fieldsErrors: string[]
-    resultCode: number
-}
-
-export type ProfileUserResponseType = {
-    userId: number
-    aboutMe: string
-    contacts: {
-        github: string
-        vk: string
-        facebook: string
-        instagram: string
-        twitter: string
-        website: string
-        youtube: string
-        mainLink: string
-    }
-    fullName: string
-    lookingForAJob: boolean
-    lookingForAJobDescription: string
-    photos: {
-        large: string
-        small: string
-    }
-}
-
-export type DefaultResponseType = {
-    resultCode: 1 | 0
-    messages: string[],
-    data: object
-}
+import {DefaultResponseType, ProfileUserResponseType, ResponseAuth, ResponseUsersType} from "api/api.types";
 
 const instance = axios.create(
     {
---
+++ src/api/api.types.ts
@@ -0,0 +1,46 @@
+import {UserType} from "redux/usersReducer";
+
+export type ResponseUsersType = {
+    items: UserType[]
+    totalCount: number
+    error: null | number
+}
+
+export type ResponseAuth = {
+    data: {
+        id: number
+        login: string
+        email: string
+    }
+    messages: string[]
+    fieldsErrors: string[]
+    resultCode: number
+}
+
+ export type ProfileUserResponseType = {
+    userId: number
+    aboutMe: string
+    contacts: {
+        github: string
+        vk: string
+        facebook: string
+        instagram: string
+        twitter: string
+        website: string
+        youtube: string
+        mainLink: string
+    }
+    fullName: string
+    lookingForAJob: boolean
+    lookingForAJobDescription: string
+    photos: {
+        large: string
+        small: string
+    }
+}
+
+export type DefaultResponseType = {
+    resultCode: 1 | 0
+    messages: string[],
+    data: object
+}

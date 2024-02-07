7bb22dee4499a47d266301768a4447b07648b95f
Aleksey <easy.mail.to.copy@gmail.com>
2024-02-07 23:08:24 +0300

    fix: Checkbox component is now controlled

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -2,7 +2,7 @@ import s from './LoginForm.module.scss'
 
 import {TextField} from "components/TextField";
 import {Button} from "components/Button";
-import {useForm} from "react-hook-form";
+import {useController, useForm} from "react-hook-form";
 import {login} from "redux/authReducer";
 import {PropertiesLogin} from "api/api.types";
 import {useAppDispatch} from "redux/reduxStore";
@@ -16,17 +16,25 @@ type FormValues = {
 
 export const LoginForm = () => {
     const dispatch = useAppDispatch()
-    const {register, handleSubmit, formState: {errors}} = useForm<FormValues>()
+    const {control, register, handleSubmit, formState: {errors}} = useForm<FormValues>()
 
     const onSubmit = (data: PropertiesLogin) => {
         dispatch((login(data)))
     }
 
+    const {
+        field: {value, onChange}
+    } = useController({
+        name: 'rememberMe',
+        control,
+        defaultValue: false,
+    })
+
     return (
         <form className={s.loginForm} onSubmit={handleSubmit(onSubmit)}>
             <TextField label={'Email'} {...register('email', {required: true})}></TextField>
-            <TextField label={'Password'} {...register('password', {required: true})}></TextField>
-            <Checkbox label={'Remember me'} {...register('rememberMe')}/>
+            <TextField type={"password"} label={'Password'} {...register('password', {required: true})}></TextField>
+            <Checkbox label={'Remember me'} {...register('rememberMe')} onValueChange={onChange} checked={value}/>
             <Button className={s.button} type='submit' fullWidth>Submit</Button>
         </form>
     )

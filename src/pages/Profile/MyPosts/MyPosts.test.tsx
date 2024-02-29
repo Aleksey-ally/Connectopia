74d46f34d5732ff59ee9a809e0ae427376b1ace1
Aleksey <easy.mail.to.copy@gmail.com>
2024-02-29 22:53:29 +0300

    feat: added test for MyPost component (Post should be an added)

--- src/pages/Profile/MyPosts/MyPosts.test.tsx
+++ src/pages/Profile/MyPosts/MyPosts.test.tsx
@@ -1,36 +1,53 @@
 import {MyPosts} from "pages/Profile/MyPosts/MyPosts";
 import {render} from "@testing-library/react";
-import {Provider} from "react-redux";
-import {reducers} from "redux/reduxStore";
-import {legacy_createStore} from "redux";
+import {create} from "react-test-renderer";
 
+let startState = {
+    postData: [
+        {id: 1, message: "Good luck!", likeCounter: 91}
+    ],
+    textPost: "",
+    profile: {},
+    status: ""
+}
 
 describe('MyPost component', () => {
-    const startState = {
-        postData: [
-            {id: 1, message: "Good luck!", likeCounter: 91}
-        ],
-        textPost: "",
-        profile: {},
-        status: ""
-    }
-
 
     test('Text post should be a changed', () => {
-        const store = legacy_createStore(reducers);
 
         const {getByPlaceholderText} = render(
-            <Provider store={store}>
-                <MyPosts profileData={startState} dispatchNewTextInput={() => {
-                }} addPost={() => {
-                }}/>
-            </Provider>
+            <MyPosts profileData={startState} dispatchNewTextInput={() => {
+            }} addPost={() => {
+            }}/>
         );
 
         const textarea = getByPlaceholderText('type text');
         textarea.innerHTML = 'Hey'
 
         expect(textarea.innerHTML).toBe('Hey')
+
+    })
+
+    test('Post should be an added', () => {
+
+        const newPost = {id: 2, message: "Good bye", likeCounter: 12}
+
+        const addPost = () => {
+            return startState = {...startState, postData: [newPost, ...startState.postData]}
+        }
+
+        const component = create(<MyPosts profileData={startState} dispatchNewTextInput={() => {
+        }} addPost={addPost}/>)
+
+        const button = component.root.findByType('button').props
+        button.onClick()
+
+        const componentWithNewPost = create(<MyPosts profileData={startState} dispatchNewTextInput={() => {
+        }} addPost={addPost}/>)
+
+        const span = componentWithNewPost.root.findAllByType('span')[0].props;
+
+        expect(span.children).toBe("Good bye")
 
     })
 

8f97dc9d09fe723b271ea23cc423fcd7ba2360a6
Aleksey <easy.mail.to.copy@gmail.com>
2024-03-07 21:12:47 +0300

    feat: added lazy loading for Profile

--- src/pages/Profile/Profile.tsx
+++ src/pages/Profile/Profile.tsx
@@ -1,13 +1,23 @@
-import {MyPostsContainer} from "./MyPosts";
-import {ProfileInformationContainer} from './ProfileInformation';
-import React from "react";
+import {lazy, Suspense} from "react";
+import {Preloader} from "components/Preloader";
+
+
+const ProfileInformationContainer = lazy(() => import('./ProfileInformation').then(module => ({default: module.ProfileInformationContainer})));
+const MyPostsContainer = lazy(() => import('./MyPosts').then(module => ({default: module.MyPostsContainer})));
 
 export const Profile = () => {
 
     return (
         <div>
-            <ProfileInformationContainer/>
-            <MyPostsContainer/>
+
+            <Suspense fallback={<Preloader/>}>
+                <ProfileInformationContainer/>
+            </Suspense>
+
+            <Suspense fallback={<Preloader/>}>
+                <MyPostsContainer/>
+            </Suspense>
+
         </div>
     )
 }

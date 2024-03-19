966e57709f991414a40fc013d77d5646ea845880
Aleksey <easy.mail.to.copy@gmail.com>
2024-03-19 21:35:00 +0300

    feat: display all data from server for profile information

--- src/pages/Profile/ProfileInformation/ProfileInformation.tsx
+++ src/pages/Profile/ProfileInformation/ProfileInformation.tsx
@@ -4,7 +4,7 @@ import UserCover from 'imgs/userCover_1.jpg';
 import s from './ProfileInformation.module.css';
 import {Typography} from "components/Typography";
 import {TextField} from "components/TextField";
-import {ChangeEvent, memo} from "react";
+import {ChangeEvent, memo, ReactNode} from "react";
 import {AppThunkDispatch} from "redux/reduxStore";
 
 type Props = {
@@ -27,7 +27,7 @@ export const ProfileInformation = memo(({
                                             dispatch
                                         }: Props) => {
     const userAvatarSelected = (e: ChangeEvent<HTMLInputElement>) => {
-        if (e.currentTarget.files){
+        if (e.currentTarget.files) {
             dispatch(setNewUserAvatar(e.currentTarget.files[0]))
         }
     }
@@ -42,12 +42,43 @@ export const ProfileInformation = memo(({
                     <UserAvatar className={s.userAvatar} size={'medium'} photos={profile?.photos?.small}/>
                     {!uID && <input type="file" onChange={userAvatarSelected}/>}
                     <div className={s.userInfoBody}>
-                        <Typography variant={'h3'}>{profile?.fullName}</Typography>
+                        <div>
+                            <b>Name: </b>
+                            <Typography variant={'h3'} as={'span'}>{profile?.fullName}</Typography>
+                        </div>
                         {edit &&
-                            <TextField onBlur={toggleEditHandler} autoFocus value={status}
-                                       onChange={changeStatusHandler}/>}
+                            <div>
+                                <b>Status: </b>
+                                <TextField onBlur={toggleEditHandler} autoFocus value={status}
+                                           onChange={changeStatusHandler}/>
+                            </div>
+                        }
+
                         {!edit &&
-                            <Typography variant={'subtitle2'} onDoubleClick={toggleEditHandler}>{status}</Typography>}
+                            <div>
+                                <b>Status: </b>
+                                <Typography variant={'subtitle2'}
+                                            as={'span'}
+                                            onDoubleClick={toggleEditHandler}>{status}</Typography>
+                            </div>
+                        }
+                        <div>
+                            <b>Looking for a job:</b> <Typography variant={'subtitle2'}
+                                                                  as={'span'}>{profile?.lookingForAJob}</Typography>
+                        </div>
+                        <div>
+                            <b>My professional skills:</b> <Typography variant={'subtitle2'}
+                                                                       as={'span'}>{profile?.lookingForAJobDescription}</Typography>
+                        </div>
+                        <div>
+                            <b>About me: </b> <Typography variant={'subtitle2'}
+                                                          as={'span'}>{profile?.aboutMe}</Typography>
+                        </div>
+                        <div>
+                            <b>Contacts: </b>{(profile?.contacts && Object.keys(profile.contacts).map(c => (
+                            <Typography key={c}>{profile.contacts[c] || 'не указано'}</Typography>
+                        ))) || null}
+                        </div>
                     </div>
 
                 </div>

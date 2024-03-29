65c177bb94e6acafc3f01ffa27f1b5504c45a2e0
Aleksey <easy.mail.to.copy@gmail.com>
2024-03-29 21:33:01 +0300

    fix: added ref for Checkbox

--- src/components/Checkbox/Checkbox.tsx
+++ src/components/Checkbox/Checkbox.tsx
@@ -2,46 +2,48 @@ import * as CheckboxRadix from '@radix-ui/react-checkbox'
 
 import s from 'components/Checkbox/Checkbox.module.scss'
 
-import { CheckMark } from 'assets/icons'
+import {CheckMark} from 'assets/icons'
 import {Typography} from "components/Typography";
-import {ReactNode} from "react";
+import {ElementRef, forwardRef, ReactNode} from "react";
 
 export type CheckboxProps = {
-  className?: string
-  label?: string | ReactNode
-  checked?: boolean
-  onValueChange?: (checked: boolean) => void
-  disabled?: boolean
-  required?: boolean
+    className?: string
+    label?: string | ReactNode
+    checked?: boolean
+    onValueChange?: (checked: boolean) => void
+    disabled?: boolean
+    required?: boolean
 }
 
-export const Checkbox = ({
-  className = '',
-  label,
-  checked,
-  onValueChange,
-  disabled,
-  required,
-}: CheckboxProps) => {
-  const classNames = {
-    label: `${s.label} ${disabled ? s.labelDisabled : ''}`,
-    checkBox: `${s.default} ${!checked ? s.uncheck : ''} ${className}`,
-  }
+export const Checkbox = forwardRef<ElementRef<typeof CheckboxRadix.Root>, CheckboxProps>((
+    {
+        className = '',
+        label,
+        checked,
+        onValueChange,
+        disabled,
+        required
+    }, ref) => {
+    const classNames = {
+        label: `${s.label} ${disabled ? s.labelDisabled : ''}`,
+        checkBox: `${s.default} ${!checked ? s.uncheck : ''} ${className}`,
+    }
 
-  return (
-    <Typography className={classNames.label} as={'label'} variant={'body2'}>
-      <CheckboxRadix.Root
-        className={classNames.checkBox}
-        checked={checked}
-        onCheckedChange={onValueChange}
-        disabled={disabled}
-        required={required}
-      >
-        <CheckboxRadix.Indicator>
-          {checked && <CheckMark disabled={disabled} />}
-        </CheckboxRadix.Indicator>
-      </CheckboxRadix.Root>
-      {label}
-    </Typography>
-  )
-}
+    return (
+        <Typography className={classNames.label} as={'label'} variant={'body2'}>
+            <CheckboxRadix.Root
+                ref={ref}
+                className={classNames.checkBox}
+                checked={checked}
+                onCheckedChange={onValueChange}
+                disabled={disabled}
+                required={required}
+            >
+                <CheckboxRadix.Indicator>
+                    {checked && <CheckMark disabled={disabled}/>}
+                </CheckboxRadix.Indicator>
+            </CheckboxRadix.Root>
+            {label}
+        </Typography>
+    )
+})

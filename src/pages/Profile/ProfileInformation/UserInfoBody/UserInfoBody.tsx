85e69161594b6b23b3c4fa40bd8013a37e9992cf
Aleksey <easy.mail.to.copy@gmail.com>
2024-04-21 20:55:23 +0300

    refactor: changed  display personal info in form of a table

--- src/pages/Profile/ProfileInformation/UserInfoBody/UserInfoBody.tsx
+++ src/pages/Profile/ProfileInformation/UserInfoBody/UserInfoBody.tsx
@@ -27,42 +27,58 @@ export const UserInfoBody = memo(({
     return (
         <div className={s.userInfoBody}>
             <Button onClick={() => setEditForm(true)}>Change</Button>
-            <div>
-                <b>Name: </b>
-                <Typography variant={'h3'} as={'span'} id={'fullName'}>{profile?.fullName}</Typography>
-            </div>
-            {edit &&
-                <div>
-                    <b>Status: </b>
-                    <TextField onBlur={toggleEditHandler} autoFocus value={status}
-                               onChange={changeStatusHandler}/>
-                </div>
-            }
-            {!edit &&
-                <div>
-                    <b>Status: </b>
-                    <Typography variant={'subtitle2'}
-                                as={'span'}
-                                onDoubleClick={toggleEditHandler}>{status}</Typography>
-                </div>
-            }
-            <div>
-                <b>Looking for a job:</b> <Typography variant={'subtitle2'}
-                                                      as={'span'}>{profile?.lookingForAJob}</Typography>
-            </div>а
-            <div>
-                <b>My professional skills:</b> <Typography variant={'subtitle2'}
-                                                           as={'span'}>{profile?.lookingForAJobDescription}</Typography>
-            </div>
-            <div>
-                <b>About me: </b> <Typography variant={'subtitle2'}
-                                              as={'span'}>{profile?.aboutMe}</Typography>
-            </div>
-            <div>
-                <b>Contacts: </b>{(profile?.contacts && Object.keys(profile.contacts).map(c => (
-                <Typography key={c}>{profile.contacts[c] || 'не указано'}</Typography>
-            ))) || null}
-            </div>
+            <ul className={s.personalInfoTable}>
+                <li>
+                    <div>
+                        <b>Name: </b>
+                        <Typography variant={'h3'} as={'span'} id={'fullName'}>{profile?.fullName}</Typography>
+                    </div>
+                </li>
+                <li>
+                    {edit &&
+                        <div>
+                            <b>Status: </b>
+                            <TextField onBlur={toggleEditHandler} autoFocus value={status}
+                                       onChange={changeStatusHandler}/>
+                        </div>
+                    }
+                    {!edit &&
+                        <div>
+                            <b>Status: </b>
+                            <Typography variant={'subtitle2'}
+                                        as={'span'}
+                                        onDoubleClick={toggleEditHandler}>{status}</Typography>
+                        </div>
+                    }
+                </li>
+                <li>
+                    <div>
+                        <b>Looking for a job:</b> <Typography variant={'subtitle2'}
+                                                              as={'span'}>{profile?.lookingForAJob}</Typography>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <b>My professional skills:</b> <Typography variant={'subtitle2'}
+                                                                   as={'span'}>{profile?.lookingForAJobDescription}</Typography>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <b>About me: </b> <Typography variant={'subtitle2'}
+                                                      as={'span'}>{profile?.aboutMe}</Typography>
+                    </div>
+                </li>
+                <li>
+                    <div>
+                        <b>Contacts: </b>{(profile?.contacts && Object.keys(profile.contacts).map(c => (
+                        <Typography key={c}>{profile.contacts[c] || 'не указано'}</Typography>
+                    ))) || null}
+                    </div>
+                </li>
+
+            </ul>
+
 
         </div>
     )

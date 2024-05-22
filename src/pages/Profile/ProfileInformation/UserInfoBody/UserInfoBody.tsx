aff06d2f1ffb6dc1f390d3ef69ef162bfd387744
Aleksey <easy.mail.to.copy@gmail.com>
2024-05-22 22:24:49 +0300

    feat: added onDoubleClick for all labels

--- src/pages/Profile/ProfileInformation/UserInfoBody/UserInfoBody.tsx
+++ src/pages/Profile/ProfileInformation/UserInfoBody/UserInfoBody.tsx
@@ -29,32 +29,32 @@ export const UserInfoBody = memo(({
             <ul className={s.personalInfoTable}>
                 <li>
                     <div>
-                        <label>Name: </label>
+                        <label onDoubleClick={enableEditForm}>Name: </label>
                         <Typography variant={'h3'} as={'div'} id={'fullName'}>{profile?.fullName}</Typography>
                     </div>
                 </li>
 
                 <li>
                     <div>
-                        <label>Looking for a job:</label> <Typography variant={'subtitle2'}
+                        <label onDoubleClick={enableEditForm}>Looking for a job:</label> <Typography variant={'subtitle2'}
                                                                       as={'div'}>{profile?.lookingForAJob}</Typography>
                     </div>
                 </li>
                 <li>
                     <div>
-                        <label>My professional skills:</label> <Typography variant={'subtitle2'}
+                        <label onDoubleClick={enableEditForm}>My professional skills:</label> <Typography variant={'subtitle2'}
                                                                            as={'div'}>{profile?.lookingForAJobDescription}</Typography>
                     </div>
                 </li>
                 <li>
                     <div>
-                        <label>About me: </label> <Typography variant={'subtitle2'}
+                        <label onDoubleClick={enableEditForm}>About me: </label> <Typography variant={'subtitle2'}
                                                               as={'div'}>{profile?.aboutMe}</Typography>
                     </div>
                 </li>
                 <li>
                     <div>
-                        <label>Contacts: </label>{(profile?.contacts && Object.keys(profile.contacts).map(c => (
+                        <label onDoubleClick={enableEditForm}>Contacts: </label>{(profile?.contacts && Object.keys(profile.contacts).map(c => (
                         <Typography key={c}>{profile.contacts[c] || 'не указано'}</Typography>
                     ))) || null}
                     </div>

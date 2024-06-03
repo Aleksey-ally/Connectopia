4940b2e76fde5d3e6a524654240d6e3fedca7f08
Aleksey <easy.mail.to.copy@gmail.com>
2024-06-03 21:59:17 +0300

    feat: added enableEditForm for all options in personal info

--- src/pages/Profile/ProfileInformation/UserInfoBody/UserInfoBody.tsx
+++ src/pages/Profile/ProfileInformation/UserInfoBody/UserInfoBody.tsx
@@ -30,7 +30,7 @@ export const UserInfoBody = memo(({
                 <li>
                     <div>
                         <label className={s.titleOption} onDoubleClick={enableEditForm}>Name: </label>
-                        <Typography variant={'h3'} as={'div'} id={'fullName'}>{profile?.fullName}</Typography>
+                        <Typography variant={'h3'} as={'div'} id={'fullName'} onDoubleClick={enableEditForm}>{profile?.fullName}</Typography>
                     </div>
                 </li>
 
@@ -43,19 +43,19 @@ export const UserInfoBody = memo(({
                 <li>
                     <div>
                         <label className={s.titleOption} onDoubleClick={enableEditForm}>My professional skills:</label> <Typography variant={'subtitle2'}
-                                                                           as={'div'}>{profile?.lookingForAJobDescription}</Typography>
+                                                                           as={'div'} onDoubleClick={enableEditForm}>{profile?.lookingForAJobDescription}</Typography>
                     </div>
                 </li>
                 <li>
                     <div>
                         <label className={s.titleOption} onDoubleClick={enableEditForm}>About me: </label> <Typography variant={'subtitle2'}
-                                                              as={'div'}>{profile?.aboutMe}</Typography>
+                                                              as={'div'} onDoubleClick={enableEditForm}>{profile?.aboutMe}</Typography>
                     </div>
                 </li>
                 <li>
                     <div>
                         <label className={s.titleOption} onDoubleClick={enableEditForm}>Contacts: </label>{(profile?.contacts && Object.keys(profile.contacts).map(c => (
-                        <Typography key={c}>{profile.contacts[c] || 'не указано'}</Typography>
+                        <Typography key={c} onDoubleClick={enableEditForm}>{profile.contacts[c] || 'не указано'}</Typography>
                     ))) || null}
                     </div>
                 </li>

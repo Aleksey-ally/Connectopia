75c168285ad8d27851b0edd4fd074583a0951b70
Aleksey <easy.mail.to.copy@gmail.com>
2024-06-14 22:18:50 +0300

    refactor: changed styles for UserInfoBodyForm

--- src/pages/Profile/ProfileInformation/UserInfoBodyForm/UserInfoBodyForm.tsx
+++ src/pages/Profile/ProfileInformation/UserInfoBodyForm/UserInfoBodyForm.tsx
@@ -40,40 +40,44 @@ export const UserInfoBodyForm = memo(({
 
     return (
         <form className={classes.form} onSubmit={handleSubmit(handleSubmitProfileForm)}>
-
             <div className={s.title}>
                 <Typography>Personal Information</Typography>
                 <Button variant={'tertiary'}>Save</Button>
             </div>
 
-            <div>
-                <label className={s.titleOption} htmlFor='fullName'><b>Name: </b></label>
-                <TextField id='fullName' {...register('fullName')} defaultValue={profile?.fullName}/>
-            </div>
-            <div>
-                <Checkbox label={<b>Looking for a job:</b>} {...register('lookingForAJob')}
-                          onValueChange={field.onChange}
-                          checked={field.value}/>
-            </div>
-            <div>
-                <label className={s.titleOption} htmlFor="lookingForAJobDescription"><b>My professional skills:</b></label><TextField id='lookingForAJobDescription' {...register('lookingForAJobDescription')}
-                                                         defaultValue={profile?.lookingForAJobDescription}/>
-            </div>
-            <div>
-                <label className={s.titleOption} htmlFor="aboutMe"><b>About me: </b></label> <TextField id='aboutMe' {...register('aboutMe')}
-                                                                              defaultValue={profile?.aboutMe}/>
-            </div>
-            <div>
-                <label className={s.titleOption} htmlFor="contacts"><b>Contacts: </b></label>
-                {Object.entries(profile?.contacts || {}).map(([key, value]) => (
-                    <TextField
-                        id='contacts'
-                        key={key}
-                        {...register(`contacts.${key}` as keyof ProfileUserResponseType)}
-                        defaultValue={value || 'не указано'}
-                    />
-                ))}
-            </div>
+            <ul className={s.personalInfoTable}>
+                <li>
+                    <label className={s.titleOption} htmlFor='fullName'><b>Name: </b></label>
+                    <TextField id='fullName' {...register('fullName')} defaultValue={profile?.fullName}/>
+                </li>
+                <li>
+                    <Checkbox label={<b>Looking for a job:</b>} {...register('lookingForAJob')}
+                              onValueChange={field.onChange}
+                              checked={field.value}/>
+                </li>
+                <li>
+                    <label className={s.titleOption} htmlFor="lookingForAJobDescription"><b>My professional skills:</b></label><TextField
+                    id='lookingForAJobDescription' {...register('lookingForAJobDescription')}
+                    defaultValue={profile?.lookingForAJobDescription}/>
+                </li>
+                <li>
+                    <label className={s.titleOption} htmlFor="aboutMe"><b>About me: </b></label> <TextField
+                    id='aboutMe' {...register('aboutMe')}
+                    defaultValue={profile?.aboutMe}/>
+                </li>
+                <li>
+                    <label className={s.titleOption} htmlFor="contacts"><b>Contacts: </b></label>
+                    {Object.entries(profile?.contacts || {}).map(([key, value]) => (
+                        <TextField
+                            id='contacts'
+                            key={key}
+                            {...register(`contacts.${key}` as keyof ProfileUserResponseType)}
+                            defaultValue={value || 'не указано'}
+                        />
+                    ))}
+                </li>
+            </ul>
+
             {errorMessage && errorMessage.map((mes) => {
                 return <div className={s.errorMessage}>{mes}</div>
             })}

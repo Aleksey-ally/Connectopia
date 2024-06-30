71ccc85452e459e153cf804a22969aecce90cb30
Aleksey <easy.mail.to.copy@gmail.com>
2024-06-30 22:24:27 +0300

    feat: added ToastifyOptions

---
+++ src/utils/ToastifyOptions/ToastifyOptions.ts
@@ -0,0 +1,22 @@
+import 'react-toastify/dist/ReactToastify.min.css'
+import s from './ToastifyOptions.module.scss'
+
+import 'animate.css/animate.min.css'
+
+export const infoOptions = {
+    style: {background: 'var(--color-dark-700)'},
+    progressStyle: {background: 'var(--color-info-700)'},
+    className: s.infoIcon,
+}
+
+export const successOptions = {
+    style: {background: 'var(--color-dark-700)'},
+    progressStyle: {background: 'var(--color-success-700)'},
+    className: s.successIcon,
+}
+
+export const errorOptions = {
+    style: {background: 'var(--color-dark-700)'},
+    progressStyle: {background: 'var(--color-danger-500)'},
+    className: s.errorIcon,
+}

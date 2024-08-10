7467e169efa80444378f8ed010b626582e0fb3c5
Aleksey <easy.mail.to.copy@gmail.com>
2024-08-10 21:32:52 +0300

    fix: solved issue display user without avatar in message page

--- src/pages/Messages/DialogUser/DialogUser.tsx
+++ src/pages/Messages/DialogUser/DialogUser.tsx
@@ -2,14 +2,16 @@ import React, {memo} from "react";
 import {NavLink} from 'react-router-dom'
 import s from 'pages/Messages/DialogUser/DialogUser.module.scss'
 import {UsersDataType} from "redux/messagesReducer";
+import {UserAvatar} from "components/UserAvatar";
 
 
 export const DialogUser = memo(({id, animalName, photoAvatar}: UsersDataType) => {
     return (
         <div className={s.dialogItem}>
             <NavLink to={`/messages/${id}`}
-                     className={navData => navData.isActive ? s.activeDialogItem : s.dialogItem}> <img src={photoAvatar}
-                                                                                                       alt={`${photoAvatar } avatar`}/>
+                     className={navData => navData.isActive ? s.activeDialogItem : s.dialogItem}>
+                <UserAvatar size={'small'} alt={`${photoAvatar } avatar`}/>
+                {/*<img src={photoAvatar} alt={`${photoAvatar } avatar`}/>*/}
                 <span>{animalName}</span></NavLink>
         </div>
     )

e4a0efae05658a7399b345240a262a28b7e87b85
Aleksey <easy.mail.to.copy@gmail.com>
2024-08-11 20:50:50 +0300

    feat: added alt for UserAvatar component

--- src/components/UserAvatar/UserAvatar.tsx
+++ src/components/UserAvatar/UserAvatar.tsx
@@ -5,13 +5,14 @@ import {ComponentPropsWithoutRef, ElementRef, forwardRef, memo} from "react";
 type Props = {
     photos?: string | null
     size: 'medium' | 'small' | 'large'
+    alt?: string
 } & ComponentPropsWithoutRef<'img'>
 
 export const UserAvatar = memo(forwardRef<ElementRef<'img'>, Props>(
-    ({photos, size, className = '', children, ...rest}, ref) => {
+    ({photos, size, alt, className = '', children, ...rest}, ref) => {
         return <div className={className}>
             <img ref={ref} className={`${s.avatar} ${s[size]}`} {...rest}
-                 src={photos || AvatarUnknownUser} alt="User avatar"/>
+                 src={photos || AvatarUnknownUser} alt={alt ? alt : "User avatar"}/>
             {children}
         </div>
     }))
